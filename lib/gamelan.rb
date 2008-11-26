69227f6a9d93326cb2629eb9c0847bfa7b0a97b0
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2008-11-25 20:24:35 -0800

    Added an MIT-LICENSE and a copyright header.

--- lib/gamelan.rb
+++ lib/gamelan.rb
@@ -1,1 +1,8 @@
+# Gamelan is a good-enough soft real-time event scheduler, 
+# written in Ruby, especially for music applications.
+#
+# Copyright (c) 2008 Jeremy Voorhis <jvoorhis@gmail.com>
+#
+# This code released under the terms of the MIT license.
+
 require 'gamelan/scheduler'
